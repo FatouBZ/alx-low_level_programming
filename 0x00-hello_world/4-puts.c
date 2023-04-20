a4a9e7b88609ec8e15c6512e25c90deaf321b8cb
FatouBZ <fbzongo@st.ug.edu.gh>
2023-04-20 16:41:55 -0700

    4- Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
 * description - Write a C program that prints exactly *
 * Return: Always 0 (Success)
 */
-int main(void) {
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
