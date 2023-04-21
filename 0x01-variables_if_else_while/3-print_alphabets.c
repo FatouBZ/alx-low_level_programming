104f0e26b2531de8d12395a29d8f0d44406ba0cb
FatouBZ <fbzongo@st.ug.edu.gh>
2023-04-21 16:45:18 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,6 +6,7 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 char letter;
@@ -15,4 +16,4 @@ for (letter = 'A'; letter <= 'Z'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
-} 
+}
