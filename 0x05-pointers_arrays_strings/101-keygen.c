4d59d19a24ef8b064a1b5c1108250ad78558132f
FatouBZ <fbzongo@st.ug.edu.gh>
2023-05-02 19:31:28 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -29,6 +29,8 @@ if (password[index] >= (33 + diff_half1))
 {
 password[index] -= diff_half1;
 break;
+}
+}
 for (index = 0; password[index]; index++)
 {
 if (password[index] >= (33 + diff_half2))
@@ -40,6 +42,4 @@ break;
 }
 printf("%s", password);
 return (0);
-}
-}
 }
